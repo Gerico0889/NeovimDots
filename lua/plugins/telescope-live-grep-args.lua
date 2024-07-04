9dc512a00b75e54fcfe626bc76b79b4d8c84ab7b
Luigi Zaccone <luigi.zaccone@egym.com>
2024-07-04 16:45:00 +0200

    Move telescope config where it actually matters

--- lua/plugins/telescope-live-grep-args.lua
+++ lua/plugins/telescope-live-grep-args.lua
@@ -9,6 +9,26 @@ return {
     },
   },
   config = function()
-    require("telescope").load_extension("live_grep_args")
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+    telescope.setup({
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      },
+    })
+    telescope.load_extension("live_grep_args")
   end,
 }
