b48a83b559e35c37cb5317718818bf82b23a740f
Luigi Zaccone <luigi.zaccone@egym.com>
2025-06-11 15:26:40 +0200

    Remove category

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local wk = require("which-key")
 local map = vim.keymap.set
-
--- Add more subcategories here
-wk.add({
-  { "<leader>m", group = "Machine GUI" },
-})
 
 -- Open custom config file
 function OpenCustomConfig()
